0eee11dce7c37130b67e5343d5c5f8231a253df9
Kabindra Shrestha <kabindra.shrestha.0wn@gmail.com>
2021-05-29 22:37:00 +0545

    Worked on animation

--- app/src/main/java/com/kabindra/animations/MainActivity.kt
+++ app/src/main/java/com/kabindra/animations/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kabindra.animations
 
 import android.os.Bundle
 import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,11 +12,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val animTopToBottom = AnimationUtils.loadAnimation(this, R.anim.anim_top_to_bottom)
+        val animScaleToBit = AnimationUtils.loadAnimation(this, R.anim.anim_scale_to_bit)
 
         val tvHeaderTitle = findViewById<TextView>(R.id.tv_header_title)
         val tvSubTitle = findViewById<TextView>(R.id.tv_sub_title)
+        val ivMainLogo = findViewById<ImageView>(R.id.iv_main_logo)
 
         tvHeaderTitle.startAnimation(animTopToBottom)
         tvSubTitle.startAnimation(animTopToBottom)
+        ivMainLogo.startAnimation(animScaleToBit)
     }
 }
